f787c287c051cc781900820c9d82eb2fb4e6e33a
ananditasinghh <97512151+ananditasinghh@users.noreply.github.com>
2023-07-08 02:14:53 +0530

    Create arrayQ6.cpp

---
+++ arrayQ6.cpp
@@ -0,0 +1,23 @@
+Best Time to Buy and Sell Stock
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.size(); i++) {
+            int currentProfit = prices[i] - minPrice;
+            maxProfit = max(maxProfit, currentProfit);
+            minPrice = min(minPrice, prices[i]);
+        }
+
+        return maxProfit;
+    }
+};
